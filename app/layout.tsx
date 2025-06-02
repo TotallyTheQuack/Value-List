bdf5c322adddc80ec937057318bff3effbb5414d
Kai <thequackkh@gmail.com>
2025-06-02 14:25:31 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Value List',
+  description: 'The Value List of all time',
+  generator: 'TheQuack',
 }
 
 export default function RootLayout({
